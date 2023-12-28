7e07357d737e22015a4ab1f4eb6f917d27a44ab5
Dinesh Srini <dinbtechit@gmail.com>
2023-12-27 22:08:03 -0400

    formatting

--- src/main/kotlin/com/github/dinbtechit/firebase/language/codeFormatting/FirestoreRulesCodeStyleSettings.kt
+++ src/main/kotlin/com/github/dinbtechit/firebase/language/codeFormatting/FirestoreRulesCodeStyleSettings.kt
@@ -3,51 +3,19 @@ package com.github.dinbtechit.firebase.language.codeFormatting
 import com.github.dinbtechit.firebase.language.FirestoreRulesLanguage
 import com.intellij.application.options.CodeStyleAbstractConfigurable
 import com.intellij.application.options.CodeStyleAbstractPanel
+import com.intellij.application.options.IndentOptionsEditor
 import com.intellij.application.options.TabbedLanguageCodeStylePanel
 import com.intellij.lang.Language
 import com.intellij.psi.codeStyle.*
-
-
-class FirestoreRulesCodeStyleSettings(settings: CodeStyleSettings) :
-    CustomCodeStyleSettings("FirestoreRulesCodeStyleSettings", settings) {
-}
-
-class FirestoreRulesCodeStyleSettingsProvider() : CodeStyleSettingsProvider() {
-    override fun createCustomSettings(settings: CodeStyleSettings): CustomCodeStyleSettings? {
-        return FirestoreRulesCodeStyleSettings(settings)
-    }
-
-    override fun getConfigurableDisplayName(): String {
-        return FirestoreRulesLanguage.INSTANCE.displayName
-    }
-
-    override fun createConfigurable(
-        settings: CodeStyleSettings,
-        modelSettings: CodeStyleSettings
-    ): CodeStyleConfigurable {
-        return object : CodeStyleAbstractConfigurable(settings, modelSettings, this.configurableDisplayName) {
-            override fun createPanel(settings: CodeStyleSettings): CodeStyleAbstractPanel {
-                return FirestoreRulesCodeStyleMainPanel(currentSettings, settings)
-            }
-        }
-    }
-}
-
-class FirestoreRulesCodeStyleMainPanel(currentSettings: CodeStyleSettings?, settings: CodeStyleSettings?) :
-    TabbedLanguageCodeStylePanel(FirestoreRulesLanguage.INSTANCE, currentSettings, settings!!) {
-    override fun initTabs(settings: CodeStyleSettings?) {
-        addIndentOptionsTab(settings);
-        addSpacesTab(settings);
-        addBlankLinesTab(settings);
-        addWrappingAndBracesTab(settings);
-    }
-}
 
 
 class FirestoreLanguageCodeStyleSettingsProvider : LanguageCodeStyleSettingsProvider() {
     override fun getLanguage(): Language = FirestoreRulesLanguage.INSTANCE
 
 
+    override fun getIndentOptionsEditor(): IndentOptionsEditor {
+        return FirestoreCodeStyleIndentOptionsEditor()
+    }
     override fun getCodeSample(p0: SettingsType): String {
         return """
          // Sample Code
@@ -74,3 +42,34 @@ class FirestoreLanguageCodeStyleSettingsProvider : LanguageCodeStyleSettingsProv
         }
     }
 }
+
+class FirestoreRulesCodeStyleSettingsProvider() : CodeStyleSettingsProvider() {
+    override fun createCustomSettings(settings: CodeStyleSettings): CustomCodeStyleSettings {
+        return FirestoreRulesCodeStyleSettings(settings)
+    }
+
+
+    override fun getConfigurableDisplayName(): String {
+        return FirestoreRulesLanguage.INSTANCE.displayName
+    }
+
+    override fun createConfigurable(
+        settings: CodeStyleSettings,
+        modelSettings: CodeStyleSettings
+    ): CodeStyleConfigurable {
+        return object : CodeStyleAbstractConfigurable(settings, modelSettings, this.configurableDisplayName) {
+            override fun createPanel(settings: CodeStyleSettings): CodeStyleAbstractPanel {
+                return FirestoreRulesCodeStyleMainPanel(currentSettings, settings)
+            }
+        }
+    }
+}
+
+class FirestoreRulesCodeStyleMainPanel(currentSettings: CodeStyleSettings?, settings: CodeStyleSettings?) :
+    TabbedLanguageCodeStylePanel(FirestoreRulesLanguage.INSTANCE, currentSettings, settings!!)
+
+class FirestoreRulesCodeStyleSettings(settings: CodeStyleSettings) :
+    CustomCodeStyleSettings("FirestoreRulesCodeStyleSettings", settings) {
+}
+class FirestoreCodeStyleIndentOptionsEditor: IndentOptionsEditor()
+
